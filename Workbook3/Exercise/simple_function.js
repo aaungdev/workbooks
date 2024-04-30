3b9d500678ddbc47302653067680c58a4e1f4c6e
aaungdev <dhev.code@gmail.com>
2024-04-30 10:33:07 -0400

    uploading excersies

---
+++ Workbook3/Exercise/simple_function.js
@@ -0,0 +1,20 @@
+" use strict";
+favoritePlace();
+WhyIamHere();
+favoriteThings();
+
+function favoriteThings() {
+  console.log(
+    "I am Aung Aung. \n My favorite movie is Marvel. \n I love to play Big Drums",
+  );
+}
+function WhyIamHere() {
+  var learnToCode =
+    " I joined Learning to Code program because my future called me and I answered. \n";
+  console.log(learnToCode);
+}
+function favoritePlace() {
+  var favoritePlace =
+    " My favorite place will be wherever I am belonging. The thing is eachp place has different unique things so I enjoyed all of places.\n";
+  console.log(favoritePlace);
+}
---
+++ Workbook3/demos/function.js
@@ -0,0 +1,15 @@
+function aungaung() {
+  console.log("Message");
+}
+aungaung();
+
+// look at number 1,2 link with var x
+function add(number1, number2) {
+  var result = number1 + number2;
+  console.log(result);
+}
+
+var x = 67;
+var y = 34;
+
+add(x, y);
