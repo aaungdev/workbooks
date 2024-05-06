09df5fa6e91f698aadd7efc9e5068448c474e971
aaungdev <dhev.code@gmail.com>
2024-05-06 10:23:02 -0400

    objects js

---
+++ Workbook4/js-objects/1-5.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+let employee = {
+    employeeId: "A134L",
+    name: "Ezra", 
+    jobTitle: "Theater Teacher",
+    payRate: 38.46
+};
+console.log(employee.payRate, employee.name);
