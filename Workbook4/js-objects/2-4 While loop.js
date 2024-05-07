93a58c490a544cb8baceeac9843a1df9717ee21a
aaungdev <dhev.code@gmail.com>
2024-05-07 09:25:10 -0400

    while loop

---
+++ Workbook4/js-objects/2-4 While loop.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let number = 1;
+let numberOfTimesThroughLoop = 1;
+
+while (numberOfTimesThroughLoop < 5) {
+  number = number * 2;
+  console.log(number);
+  numberOfTimesThroughLoop++;
+}
