ec17460dd188d3e47d810392bfc9878d9e28f6f3
aaungdev <dhev.code@gmail.com>
2024-05-07 11:18:52 -0400

    exercise 1 loops

--- Workbook4/LoopScripts/exercise1.js
+++ Workbook4/LoopScripts/exercise1.js
@@ -1,7 +1,11 @@
 "use strict";
 
-let dear = "I love loops";
-let i;
-for (i = 0; i < 7; i++) {
-  console.log(dear);
+// let dear = "I love loops";
+// let i;
+// for (i = 0; i < 7; i++) {
+//   console.log(dear);
+// }
+
+for (let i = 0; i < 7; i++) {
+  console.log("I love loops");
 }
