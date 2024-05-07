c7e6221c3d86945faef678fd13f6c39e0ffc3957
aaungdev <dhev.code@gmail.com>
2024-05-07 13:35:26 -0400

    finding avg scores exercise

---
+++ Workbook4/array-scripts/my-family.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let siblings = ["Aung", "Maung", "Kaung", "Haung"];
+
+for (let index = 0; index < siblings.length; index++) {
+  console.log(siblings[index]);
+}
---
+++ Workbook4/array/firstname.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let firstNames = ["Michael", "David", "Boss", "Yo"];
+
+// console.log(firstNames[1]);
+
+// for (let index = 0; index < firstNames.length; index++) {
+//   console.log(firstNames[index]);
+// }
+
+for (let firstName of firstNames) {
+  console.log(firstName);
+};
