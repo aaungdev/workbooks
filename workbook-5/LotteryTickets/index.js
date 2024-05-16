42fb5a0998a0c1411de92afde0d8ead6d232bac6
aaungdev <dhev.code@gmail.com>
2024-05-16 13:16:11 -0400

    mini project

--- workbook-5/LotteryTickets/index.js
+++ workbook-5/LotteryTickets/index.js
@@ -15,25 +15,39 @@ let winningTickets = [
 ];
 
 window.onload = function () {
-  // HTML element variables
-  const winningTicketsTblBody = document.querySelector(
-    "#winningTicketsTblBody",
-  );
 
-  //functions
-  function loadWinningTicketsTable() {
-    for (const winningTicket of winningTickets) {
-      // let row = winningTicketsTblBody.insertRow("tr");
-      // winningTicketsTblBody.appendChild(row);
-      let row = winningTicketsTblBody.insertRow(); // samething above two line rather than just one line
-      let cell1 = row.insertCell(0);
-      cell1.innerText = winningTicket.tixNum;
+    // HTML element variables
+    const winningTicketsTblBody = document.querySelector("#winningTicketsTblBody");
+  
+    //functions
+    function loadWinningTicketsTable() {
+        for (const winningTicket of winningTickets) {
+            // let row = document.createElement("tr");
+            // winningTicketsTblBody.appendChild(row);
 
-      let cell2 = row.insertCell(1);
-      cell2.innerText = winningTicket.prize;
+            // this does the same things
+            let row = winningTicketsTblBody.insertRow();
+
+            let cell1 = row.insertCell(0);
+            cell1.innerText = winningTicket.tixNum;
+
+            let cell2 = row.insertCell(1);
+            cell2.innerText = "$ " + winningTicket.prize;
+
+            let cell3 = row.insertCell(2);
+
+            cell3.innerText = winningTicket.expires;
+
+
+            
+            
+        }
+
     }
-  }
-  //event handling
-
-  //initial loading
+  
+    //event handling
+  
+    //initial loading
+    loadWinningTicketsTable();
+  
 };
