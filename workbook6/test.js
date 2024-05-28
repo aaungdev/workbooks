71e3c604f784458d7253053461c0fd5a0bf02524
aaungdev <dhev.code@gmail.com>
2024-05-28 18:38:06 -0400

    testing for test

---
+++ workbook6/test.js
@@ -0,0 +1,128 @@
+"use strict";
+// let item = document.getElementById("item").value;
+// let price;
+// switch(item)
+// {
+//   case "Hat":
+//   case "Gloves":
+//     price = 15.95;
+//     break;
+//   case "Coat":
+//     price = 49.99;
+//     break;
+//   default:
+//     price = 0;
+// }
+
+// let a = 5, b = 3, c = 4, d = 2;
+// let answer = a + b * c - d;
+// console.log(answer);
+
+// let sum = 0;
+
+// function add(x, y, z)
+// {
+//   let sum = x + y + z;
+//   return sum;
+// }
+// function display()
+// {
+//   console.log(sum);
+// }
+
+// sum = add(1, 2, 3);
+// sum = add(3, 4, 5);
+// display();
+
+// let d = new Date();
+// console.log(d.getMonth() + "/" + (d.getDay() + 1) +
+// "/" + d.getFullYear());
+
+// let d = new Date();
+// console.log((d.getMonth() + 1) + "/" + d.getDay() +
+// "/" + d.getFullYear());
+
+// let d = new Date();
+// console.log((d.getMonth() + 1) + "/" + d.getDay() +
+// "/" + d.getFullYear());
+
+// let d = new Date();
+// console.log((d.getMonth() + 1) + "/" + d.getDate() +
+// "/" + d.getFullYear());
+
+// let d = new Date();
+// console.log(d.getMonth() + "/" + (d.getDay() + 1) +
+// "/" + d.getFullYear());
+
+// if (document.getElementById("fast").checked == true)
+// displayAlert("Rush");
+// const num = 42; // Replace with your desired number
+// if (num >= 18 && num <= 65) {
+//     console.log("The number is within the range (18-65).");
+// } else {
+//     console.log("The number is outside the range (18-65).");
+// }
+
+// for(let i = 0; i < 5; i++)
+// {
+//   // do something
+// }
+
+// let colors = ["green", "yellow", "blue", "red", "pink"];
+// for(let i = 0; i < 5; i++)
+// {
+//   if (colors[i] == "blue") break;
+//   // do something
+// }
+// let colors = ["green", "yellow", "blue", "red", "pink"];
+// for(let i = 0; i < 5; i++)
+// {
+//   if (i == "blue") break;
+//   // do something
+// }
+// let colors = ["green", "yellow", "blue", "red", "pink"];
+// for(let i = 0; i < 5; i++)
+// {
+//   if (colors[i] == "blue") exit-the-loop;
+//   // do something
+// }
+
+// let colors = ["green", "yellow", "blue", "red", "pink"];
+// for(let i = 0; i < 5; i++)
+// {
+//   if (colors[i] == "blue") exitLoop();
+//   // do something
+// }
+// let colors = ["green", "yellow", "blue", "red", "pink"];
+// for(let i = 0; i < 5; i++)
+// {
+//   if (colors[i] == "7") break;
+//   // do something
+// }
+// console.log (colors);
+
+// let numbers = [0, 0, 0];
+// function doit(n)
+// {
+//   let num = 10;
+//   for (let i = 1; i < n; i++)
+//   {
+//     num += i;
+//     numbers[i] = num;
+//   }
+// }
+// doit(3);
+// console.log(numbers);
+
+/*let ticketHolder = {
+    ticketID : "GCIWMY",
+    tickholder : "Chris O'Gre",
+    seats : ["Sec E Row 22 Seat 1",
+             "Sec E Row 22 Seat 2"]
+  };
+  let amt = 0;
+  for (let i = 0; i < ticketHolder.seats.length; i++)
+  {
+    amt += 125;
+  }
+  console.log(amt);
