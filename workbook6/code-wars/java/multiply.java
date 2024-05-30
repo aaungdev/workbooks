31ec230b64f46d4e50dfcac19135773d92c77846
aaungdev <dhev.code@gmail.com>
2024-05-30 14:56:58 -0400

    multiply java

---
+++ workbook6/code-wars/java/multiply.java
@@ -0,0 +1,18 @@
+// Implement a function called multiply, which takes two numbers and returns their product:
+
+// multiply(2, 3) = 6
+// multiply(0, 188) = 0
+// multiply(85, 144) = 12240
+
+
+public class Kata {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiply(2, 3));  
+        System.out.println(multiply(0, 188));  
+        System.out.println(multiply(85, 144)); 
+    }
+}
