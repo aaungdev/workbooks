7dee8437d5f95eae4050bf99075439a955b7e49e
aaungdev <dhev.code@gmail.com>
2024-06-03 11:34:58 -0400

    exercise 1

--- workbook7/demos/api-test.js
+++ workbook7/demos/api-test.js
@@ -1,3 +1,5 @@
+"use strict"
+
 fetch("https://jsonplaceholder.typicode.com/todos/5")
   .then((response) => response.json())
   .then((json) => console.log(json));
---
+++ workbook7/exercises/exercise1.js
@@ -0,0 +1,29 @@
+"use strict"
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+//   fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+//   fetch("https://jsonplaceholder.typicode.com/albums")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// fetch("https://jsonplaceholder.typicode.com/comments")
+// .then((response) => response.json())
+// .then((json) => console.log(json));
+
+// fetch("https://jsonplaceholder.typicode.com/comments/3")
+// .then((response) => response.json())
+// .then((json) => console.log(json));
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+// .then((response) => response.json())
+// .then((json) => console.log(json));
+
+fetch("https://jsonplaceholder.typicode.com/profile")
+.then((response) => response.json())
+.then((json) => console.log(json));
