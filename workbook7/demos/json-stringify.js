dba356525fce0b21eefaee087d59b68718979696
aaungdev <dhev.code@gmail.com>
2024-06-03 10:35:09 -0400

    json stringify get started

---
+++ workbook7/demos/json-stringify.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let job = {
+  title: "Web Designer",
+  startDate: new Date(),
+  company: "AT&T",
+  minSalary: 52000,
+  maxSalary: 86000,
+};
+
+let jobAsString = JSON.stringify(job);
+console.log(jobAsString);
