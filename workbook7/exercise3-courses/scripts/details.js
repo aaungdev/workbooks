bf8cc11ae73089fd99f688f572b71db9188bba50
aaungdev <dhev.code@gmail.com>
2024-06-07 23:11:49 -0400

    exercise 3

--- workbook7/exercise3-courses/scripts/details.js
+++ workbook7/exercise3-courses/scripts/details.js
@@ -1,24 +1,29 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  let params = new URLSearchParams(window.location.search);
-  let courseId = params.get("courseid");
+  const urlParams = new URLSearchParams(location.search);
+  let id = -1;
+  if (urlParams.has("courseid") === true) {
+    id = urlParams.get("courseid");
 
-  fetch(`http://localhost:8081/api/courses/${courseId}`)
-    .then((response) => response.json())
-    .then((course) => {
-      document.getElementById("detailsDepartment").textContent =
-        course.department;
-      document.getElementById("detailsNumber").textContent = course.number;
-      document.getElementById("detailsTitle").textContent = course.title;
-      document.getElementById("detailsDescription").textContent =
-        course.description;
-      document.getElementById("detailsInstructor").textContent =
-        course.instructor;
-      document.getElementById("detailsStartDate").textContent =
-        course.startDate;
-      document.getElementById("detailsDuration").textContent =
-        course.duration + " days";
-    })
-    .catch((error) => console.error("Error:", error));
+    fetch(`http://localhost:8081/api/courses/${id}`)
+      .then((response) => response.json())
+      .then((course) => {
+        document.getElementById("detailsDepartment").textContent =
+          course.department;
+        document.getElementById("detailsNumber").textContent = course.number;
+        document.getElementById("detailsTitle").textContent = course.title;
+        document.getElementById("detailsDescription").textContent =
+          course.description;
+        document.getElementById("detailsInstructor").textContent =
+          course.instructor;
+        document.getElementById("detailsStartDate").textContent =
+          course.startDate;
+        document.getElementById("detailsDuration").textContent =
+          course.duration + " days";
+      })
+      .catch((error) => console.error("Error:", error));
+  } else {
+    console.error("Course ID not found in the query string.");
+  }
 });
