d264b7b94cd1d90be318c688d5dd009c5f266ba2
aaungdev <dhev.code@gmail.com>
2024-06-11 09:43:52 -0400

    delete js

---
+++ workbook8/exercises/user-exercise/scripts/delete.js
@@ -0,0 +1,14 @@
+function deleteUserData() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const userId = urlParams.get('id');
+
+    fetch(`http://localhost:3000/users/${userId}`, {
+        method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('User deleted:', data);
+        window.location.href = 'index.html';
+    })
+    .catch(error => console.error('Error deleting user:', error));
+}
