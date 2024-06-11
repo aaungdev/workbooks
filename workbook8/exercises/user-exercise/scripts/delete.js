c2cd10a42ecec7ab8b5f3c41780bfc58cd6a1353
aaungdev <dhev.code@gmail.com>
2024-06-11 09:48:19 -0400

    delete

--- workbook8/exercises/user-exercise/scripts/delete.js
+++ workbook8/exercises/user-exercise/scripts/delete.js
@@ -1,14 +1,16 @@
-function deleteUserData() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const userId = urlParams.get('id');
+"use strict";
 
-    fetch(`http://localhost:3000/users/${userId}`, {
-        method: 'DELETE'
+function deleteUserData() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const userId = urlParams.get("id");
+
+  fetch(`http://localhost:3000/users/${userId}`, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("User deleted:", data);
+      window.location.href = "index.html";
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log('User deleted:', data);
-        window.location.href = 'index.html';
-    })
-    .catch(error => console.error('Error deleting user:', error));
+    .catch((error) => console.error("Error deleting user:", error));
 }
