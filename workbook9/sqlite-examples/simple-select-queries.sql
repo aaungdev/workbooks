08272a4392894667b7b623e26911c596640636ef
Aung Aung <152508530+aaungdev@users.noreply.github.com>
2025-07-31 10:21:01 -0400

    Update simple-select-queries.sql

--- workbook9/sqlite-examples/simple-select-queries.sql
+++ workbook9/sqlite-examples/simple-select-queries.sql
@@ -1,3 +1,3 @@
 =SUM(FILTER(Data!W:W, (ISNUMBER(SEARCH({"RAI","OMR","BAU"}, Data!E:E))) * (Data!F:F = "M" & B2)))
-
+=SUM(FILTER(Data!W:W, (ISNUMBER(SEARCH({"RAI","OMR","BAU"}, Data!E:E))) * (Data!F:F = "M" & B2) * (Data!G:G = 2025)))
 SELECT FirstName, LastName FROM Customers WHERE LastName = 'Griffin';
