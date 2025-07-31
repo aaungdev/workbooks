853f76ce3099f1fe463a75e6374c2815f2cee08c
Aung Aung <152508530+aaungdev@users.noreply.github.com>
2025-07-31 12:41:11 -0400

    Update simple-select-queries.sql

--- workbook9/sqlite-examples/simple-select-queries.sql
+++ workbook9/sqlite-examples/simple-select-queries.sql
@@ -1,4 +1,5 @@
 =IFERROR(
-   SUMIFS(WORKDRIVER!GC:GC, WORKDRIVER!CQ:CQ, C2, WORKDRIVER!CV:CV, {"MRM Tasks","OMRs","RAIs","RMs"}) /
-   B2,
+    SUM(
+        SUMIFS('WORK DRIVER'!GC:GC, 'WORK DRIVER'!CO:CO, C13, 'WORK DRIVER'!CV:CV, {"MRM Tasks", "OMRs", "RAIs", "RMs"})
+    ) / B13,
 0)
